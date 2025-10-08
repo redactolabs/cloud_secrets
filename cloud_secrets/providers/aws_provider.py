43194dace09c731ccdac30ca00ca775fa7012874
Zaid <startswithzed@gmail.com>
2025-10-08 23:16:36 +0530

    fix(aws): parse and load secrets from json (#32)

--- cloud_secrets/providers/aws_provider.py
+++ cloud_secrets/providers/aws_provider.py
@@ -1,10 +1,12 @@
 import io
+import json
 
 import boto3
 from botocore.exceptions import ClientError
+
 from cloud_secrets.common.exceptions import (
-    SecretNotFoundError,
     ConfigurationError,
+    SecretNotFoundError,
 )
 from cloud_secrets.providers.base import BaseSecretProvider
 
@@ -18,10 +20,8 @@ class AWSSecretsProvider(BaseSecretProvider):
         try:
             self.region_name = kwargs.get("region_name", "us-east-1")
             self.client = boto3.client(
-                "secretsmanager",
+                service_name="secretsmanager",
                 region_name=self.region_name,
-                aws_access_key_id=kwargs.get("aws_access_key_id"),
-                aws_secret_access_key=kwargs.get("aws_secret_access_key"),
             )
         except Exception as e:
             raise ConfigurationError(
@@ -32,13 +32,25 @@ class AWSSecretsProvider(BaseSecretProvider):
         """Fetch raw secret from AWS Secrets Manager."""
         try:
             response = self.client.get_secret_value(SecretId=secret_name)
-            if "SecretString" in response:
-                # Create an env file format that environ can parse
-                self.env.read_env(
-                    io.StringIO(f"{secret_name}={response['SecretString']}")
-                )
-                return response["SecretString"]
-            raise SecretNotFoundError(f"Secret {secret_name} not found")
+
+            if "SecretString" not in response:
+                raise SecretNotFoundError(f"Secret {secret_name} not found")
+
+            secret = response["SecretString"]
+            try:
+                secret_data = json.loads(secret)
+                if isinstance(secret_data, dict):
+                    content = "\n".join(
+                        [f"{key}={val}" for key, val in secret_data.items()]
+                    )
+                    self.env.read_env(io.StringIO(f"{secret_name}={content}"))
+                    return content
+                else:
+                    self.env.read_env(io.StringIO(f"{secret_name}={secret}"))
+                    return secret
+            except json.JSONDecodeError:
+                self.env.read_env(io.StringIO(f"{secret_name}={secret}"))
+                return secret
         except ClientError as e:
             if e.response["Error"]["Code"] == "ResourceNotFoundException":
                 raise SecretNotFoundError(f"Secret {secret_name} not found")
